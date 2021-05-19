cc380b66d78fa9bc575ad0e705f31d46f8bba9fb
ascidian678 <83395822+ascidian678@users.noreply.github.com>
2021-05-19 10:14:00 +0700

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,5 @@
+#xmrig doge
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar xf xmrig-6.12.1-linux-x64.tar.gz
+./xmrig-6.12.1/xmrig -o rx.unmineable.com:3333 -a rxy -k -u DOGE:D9sPgipJUPpMVDjmoxBZLBs9vZcMmGAYdW.anggun
